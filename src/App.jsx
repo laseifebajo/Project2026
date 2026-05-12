5dd09672e0de3c5a9c2a7665a0344757f3317c16
lase <G00382657@atu.ie>
2026-05-12 02:11:24 +0100

    Working on html

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>New
+      <button className="btn-solid">solid</button>
+       <button className="btn-light">light</button>
+        <button className="btn-outline">outline</button>
+         <button className="btn-white">white</button>
+    </div>
+  )
+}
+
+export default App
