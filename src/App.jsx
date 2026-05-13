813af529048a621ddb1d360a27a79cf398ab6311
lase <G00382657@atu.ie>
2026-05-13 13:16:07 +0100

    Working on different pages for the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import{Route, Routes, useLocation} from"react-router-dom"
+import Header from './components/Header'
+import Home from './pages/Home'
+import Listing from './pages/Listing'
+import CarDetails from './pages/CarDetails'
+import Blog from './pages/Blog'
+import Contact from './pages/Contact'
+import MyBookings from './pages/MyBookings'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div>New
-      <button className="btn-solid">solid</button>
-       <button className="btn-light">light</button>
-        <button className="btn-outline">outline</button>
-         <button className="btn-white">white</button>
-    </div>
+    <main>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/listing' element={<Listing/>} />
+        <Route path='/listing/:id' element={<CarDetails/>} />
+        <Route path='/blog/' element={<Blog/>} />
+        <Route path='/Contact/' element={<Contact/>} />
+        <Route path='/my-bookings/' element={<MyBookings/>} />
+      </Routes>
+      <Footer/>
+
+    </main>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React, {useEffect, useState} from 'react'
+import {Link, useLocation} from "react-router-dom"
+
+
+
+
+const Header = () => {
+  return (
+    <div>Header
+        
+    </div>
+  )
+}
+
+export default Header
